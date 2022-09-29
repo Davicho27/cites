fce6ca1f1f21d0f0c8bbef886830d160250593d9
davicho27 <davidmisaelromero27032015@gmail.com>
2022-09-29 15:17:04 -0500

    Commint con poder

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Cita from './componentes/Cita';
 
 export default function App() {
+  const [citas, setCitas] = useState ([
+    {id: "1", paciente: "Hook", propietario: "Juan", sintomas: "No come"},
+    {id: "2", paciente: "NOO", propietario: "Juan", sintomas: "No come"},
+    {id: "3", paciente: "Vamos", propietario: "Juan", sintomas: "No come"}
+  ]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
-      <StatusBar style="auto" />
+      <FlatList
+      data={citas}
+      renderItem={ ({item}) => <Cita item={item}/>} 
+      keyExtractor = {cita => cita.id}
+      />  
     </View>
   );
 }
@@ -13,11 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
   titulo:{
-    marginTop
+    color: '#000',
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
+  paciente:{
+    color:'#000'
+  }
 });
