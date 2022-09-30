c315839e49bceb089f65cb447a931412937128c3
davicho27 <davidmisaelromero27032015@gmail.com>
2022-09-30 10:32:25 -0500

    Vamos con esto

--- App.js
+++ App.js
@@ -2,20 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Cita from './componentes/Cita';
+import Formulario from './componentes/Formulario';
 
 export default function App() {
   const [citas, setCitas] = useState ([
     {id: "1", paciente: "Hook", propietario: "Juan", sintomas: "No come"},
     {id: "2", paciente: "NOO", propietario: "Juan", sintomas: "No come"},
     {id: "3", paciente: "Vamos", propietario: "Juan", sintomas: "No come"}
-  ]);
+  ]);                   
+
+  const eliminarPaciente   = id => {
+    setCitas ((citasActuales) => {
+      return citasActuales.filter(cita => cita.id !== id);
+    }) 
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
+      <Formulario/>
+      <Text style={styles.titulo}>{citas.length > 0 ? 'Administra tus citas': 'No hay citas, agrega una'}</Text>
       <FlatList
       data={citas}
-      renderItem={ ({item}) => <Cita item={item}/>} 
+      renderItem={ ({item}) => <Cita item={item} eliminarPaciente={eliminarPaciente}/>} 
       keyExtractor = {cita => cita.id}
       />  
     </View>
@@ -30,6 +39,7 @@ const styles = StyleSheet.create({
   titulo:{
     color: '#000',
     marginTop: 40,
+    marginBottom: 20,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center'
